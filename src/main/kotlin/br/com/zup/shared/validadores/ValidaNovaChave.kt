14b199f7a3618daaba7f137b8c20a14190bcaff7
Jean Amadeu <jean.amadeu2@gmail.com>
2021-04-04 13:57:24 -0300

    005-registrando-uma-nova-chave-pix

--- src/main/kotlin/br/com/zup/shared/validadores/ValidaNovaChave.kt
+++ src/main/kotlin/br/com/zup/shared/validadores/ValidaNovaChave.kt
@@ -8,34 +8,34 @@ fun validaChave(novaChave: NovaChaveRequest?) {
     novaChave?.let {
         when (it.tipoChave) {
             UNKNOWN_TIPO_CHAVE -> {
-                throw IllegalArgumentException("The key type must be cpf, email, phone or random")
+                throw IllegalArgumentException("O tipo da chave deve ser cpf, email, celular ou aleatoria")
             }
 
             CPF -> {
                 if (it.chave.isNullOrBlank() || !it.chave.matches("^[0-9]{11}\$".toRegex())) {
-                    throw IllegalArgumentException("Cpf must be in the format 00000000000")
+                    throw IllegalArgumentException("O cpf deve estar no formato 00000000000")
                 }
             }
 
             CELULAR -> {
                 if (it.chave.isNullOrBlank() || !it.chave.matches("^\\+[1-9][0-9]\\d{1,14}\$".toRegex())) {
-                    throw IllegalArgumentException("Phone must be in the format +5585988714077")
+                    throw IllegalArgumentException("O celular deve estar no formato +5585988714077")
                 }
             }
 
             EMAIL -> {
                 if (it.chave.isNullOrBlank() || !EmailValidator().isValid(it.chave, null)) {
-                    throw IllegalArgumentException("Email must be in the format email@email.com")
+                    throw IllegalArgumentException("O email deve estar no formato email@email.com")
                 }
             }
 
             ALEATORIA -> {
                 if (!it.chave.isNullOrBlank()) {
-                    throw IllegalArgumentException("The key will be automatically generated")
+                    throw IllegalArgumentException("A chave ira ser gerada automaticamente")
                 }
             }
             else -> {
-                throw IllegalArgumentException("The key type must be cpf, email, phone or random")
+                throw IllegalArgumentException("O tipo da chave deve ser cpf, email, celular ou aleatoria")
             }
         }
     }
--- src/test/kotlin/br/com/zup/KeyManagerServerTest.kt
+++ src/test/kotlin/br/com/zup/KeyManagerServerTest.kt
@@ -7,6 +7,7 @@ import br.com.zup.itau.ContaItauResponse
 import br.com.zup.itau.HttpClientItau
 import br.com.zup.itau.Instituicao
 import io.grpc.ManagedChannel
+import io.grpc.StatusRuntimeException
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
@@ -14,12 +15,20 @@ import io.micronaut.grpc.server.GrpcServerChannel
 import io.micronaut.http.HttpResponse
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.function.Executable
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
+import java.lang.Exception
+import java.lang.IllegalArgumentException
 import java.util.*
 import javax.inject.Inject
 
@@ -62,6 +71,23 @@ internal class KeyManagerServerTest(
         }
     }
 
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = ["cpfInvalido"])
+    fun `Nao registra nova chave pix cpf esta com formato invalido, vazio ou nulo `(cpf: String?) {
+        `when`(itauClient.buscaConta(clienteId = idCliente.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(contaItauResponse(idCliente)))
+
+        grpcClient.run {
+            assertThrows<StatusRuntimeException> {
+                registra(registraNovaChaveRequest(tipoChave = CPF, cpf))
+            }
+        }.also {
+            assertTrue(it.message!!.contains("O cpf deve estar no formato 00000000000"))
+        }
+    }
+
     @Test
     fun `Registra nova chave pix com EMAIL`() {
         `when`(itauClient.buscaConta(clienteId = idCliente.toString(), tipo = "CONTA_CORRENTE"))
@@ -82,6 +108,23 @@ internal class KeyManagerServerTest(
                 Executable { assertEquals("UNIBANCO ITAU SA", it.conta.instituicao.nomeInstituicao) },
                 Executable { assertEquals("Cliente", it.conta.nomeCliente) }
             )
+        }
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = ["emailInvalido"])
+    fun `Nao registra nova chave pix email esta com formato invalido, vazio ou nulo `(email: String?) {
+        `when`(itauClient.buscaConta(clienteId = idCliente.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(contaItauResponse(idCliente)))
+
+        grpcClient.run {
+            assertThrows<StatusRuntimeException> {
+                registra(registraNovaChaveRequest(tipoChave = EMAIL, email))
+            }
+        }.also {
+            assertTrue(it.message!!.contains("O email deve estar no formato email@email.com"))
         }
     }
 
@@ -108,6 +151,23 @@ internal class KeyManagerServerTest(
         }
     }
 
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = ["emailInvalido"])
+    fun `Nao registra nova chave pix celular esta com formato invalido, vazio ou nulo `(celular: String?) {
+        `when`(itauClient.buscaConta(clienteId = idCliente.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(contaItauResponse(idCliente)))
+
+        grpcClient.run {
+            assertThrows<StatusRuntimeException> {
+                registra(registraNovaChaveRequest(tipoChave = CELULAR, celular))
+            }
+        }.also {
+            assertTrue(it.message!!.contains("O celular deve estar no formato +5585988714077"))
+        }
+    }
+
     @Test
     fun `Registra nova chave pix com ALEATORIA`() {
         `when`(itauClient.buscaConta(clienteId = idCliente.toString(), tipo = "CONTA_CORRENTE"))
@@ -131,8 +191,22 @@ internal class KeyManagerServerTest(
         }
     }
 
+    @Test
+    fun `Nao registra nova chave pix quando o tipo da chave for ALEATORIA e a chave informada nao for nula`() {
+        `when`(itauClient.buscaConta(clienteId = idCliente.toString(), tipo = "CONTA_CORRENTE"))
+            .thenReturn(HttpResponse.ok(contaItauResponse(idCliente)))
+
+        grpcClient.run {
+            assertThrows<StatusRuntimeException> {
+                registra(registraNovaChaveRequest(tipoChave = ALEATORIA, "chave"))
+            }
+        }.also {
+            assertTrue(it.message!!.contains("A chave ira ser gerada automaticamente"))
+        }
+    }
+
     private fun registraNovaChaveRequest(tipoChave: TipoChave, chave: String?): RegistraNovaChaveRequest {
-        return if (tipoChave == ALEATORIA) {
+        return if (chave == null) {
             RegistraNovaChaveRequest.newBuilder()
                 .setIdCliente(idCliente.toString())
                 .setTipoChave(tipoChave)
